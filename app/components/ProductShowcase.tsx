13270975589a05640f20a03152eaa84fdefe04b7
Transcendentlabs2 <transcendent.labs2@gmail.com>
2026-02-07 12:19:12 -0500

    showcase dark6

--- app/components/ProductShowcase.tsx
+++ app/components/ProductShowcase.tsx
@@ -5,8 +5,8 @@ import { Plus, ArrowRight, ScanLine, Info } from "lucide-react";
 
 // --- IMPORTACIÓN DE IMÁGENES ---
 // Asegúrate de que las rutas sean correctas
-import imggh from '../assets/gh.webp'; 
-import imgnad from '../assets/nad.webp';
+import imggh from '../img/gh.webp'; 
+import imgnad from '../img/nad.webp';
 
 // Datos de Productos
 const PRODUCTS = [
